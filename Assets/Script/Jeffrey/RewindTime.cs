224238dd041f593f0716cca3eb29bf199f58be33
Jezyglee <jeffrey.eugne@gmail.com>
2025-02-21 00:25:03 +0100

    changement code Rewind

--- Assets/Script/Jeffrey/RewindTime.cs
+++ Assets/Script/Jeffrey/RewindTime.cs
@@ -9,6 +9,9 @@ public class RewindTime : MonoBehaviour
     [SerializeField] private bool _isRewinding = false;
     [SerializeField] List<PointInTime> _pointInTime = new List<PointInTime>();
     [SerializeField] private Rigidbody _ballRb;
+    [SerializeField] private float _speed = 2.0f; 
+
+    private int _indexPosition = 0;
     #endregion
 
 
@@ -30,15 +33,7 @@ public class RewindTime : MonoBehaviour
     #region Methods
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            //StartRewind();
-        }
-
-        if (Input.GetKeyUp(KeyCode.E))
-        {
-            StopRewind();   
-        }
+        
     }
 
     private void FixedUpdate()
@@ -52,17 +47,29 @@ public class RewindTime : MonoBehaviour
         {
             Record(); 
         }
+
+        //FakeReplayPosition();
     }
 
     private void Replay()
     {
         if (_pointInTime.Count > 0)
         {
-            PointInTime pointInTime = _pointInTime[0];
-            transform.position = pointInTime._position; 
-            transform.rotation = pointInTime._rotation;
-            _pointInTime.RemoveAt(0);
-           
+
+            PointInTime pointInTime = _pointInTime[_indexPosition];
+
+            while(Vector3.Distance(transform.position, pointInTime._position) > 0.1f)
+            {
+                transform.position = pointInTime._position;
+                transform.rotation = pointInTime._rotation;    
+                _indexPosition++;
+                _indexPosition = Mathf.Clamp(_indexPosition, 0, _pointInTime.Count - 1);
+            }
+
+            //_pointInTime.RemoveAt(0);
+            //transform.position = Vector3.Lerp(transform.position, pointInTime._position, Time.fixedDeltaTime);
+            //transform.rotation = pointInTime._rotation;
+
         }
 
         else
@@ -73,7 +80,37 @@ public class RewindTime : MonoBehaviour
 
     private void Record()
     {
+        if (_pointInTime.Count > Mathf.Round(5f / Time.fixedDeltaTime))
+        {
+            _pointInTime.RemoveAt(0); 
+        }
+
         _pointInTime.Insert(0, new PointInTime(transform.position, transform.rotation));
+
+    }
+
+    private void FakeReplayPosition()
+    {
+
+        //_ballRb.velocity = nextPos; 
+
+        
+        //int lastIndex = _pointInTime.Count - 1;
+       
+        
+        //if (Vector3.Distance(_ballRb.velocity,_pointInTime[lastIndex]._position) < 0.1f)
+        //{
+        //    Debug.Log("You've reached your position");
+        //}
+        
+        //for (int i = 0; i < _pointInTime.Count; i++)
+        //{
+        //    Vector3 nextPos = _pointInTime[i]._position;
+        //    Quaternion nextRo = _pointInTime[i]._rotation;
+
+        //    _ballRb.velocity = Vector3.Lerp(_ballRb.velocity, nextPos, 1);
+        //}
+
     }
 
     public void StartRewind()
