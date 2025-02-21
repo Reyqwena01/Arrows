9c774df0091883d44a023c96e6c8b67ecfddd083
Jezyglee <157793047+Jezyglee@users.noreply.github.com>
2025-02-21 15:10:19 +0100

    push

--- Assets/Script/Jeffrey/RewindTime.cs
+++ Assets/Script/Jeffrey/RewindTime.cs
@@ -12,6 +12,7 @@ public class RewindTime : MonoBehaviour
     [SerializeField] private float _speed = 2.0f; 
 
     private int _indexPosition = 0;
+    private bool _canpPlayPosition = false; 
     #endregion
 
 
@@ -48,7 +49,7 @@ public class RewindTime : MonoBehaviour
             Record(); 
         }
 
-        //FakeReplayPosition();
+        FakeReplayPosition();
     }
 
     private void Replay()
@@ -57,18 +58,10 @@ public class RewindTime : MonoBehaviour
         {
 
             PointInTime pointInTime = _pointInTime[_indexPosition];
-
-            while(Vector3.Distance(transform.position, pointInTime._position) > 0.1f)
-            {
-                transform.position = pointInTime._position;
-                transform.rotation = pointInTime._rotation;    
-                _indexPosition++;
-                _indexPosition = Mathf.Clamp(_indexPosition, 0, _pointInTime.Count - 1);
-            }
-
-            //_pointInTime.RemoveAt(0);
-            //transform.position = Vector3.Lerp(transform.position, pointInTime._position, Time.fixedDeltaTime);
-            //transform.rotation = pointInTime._rotation;
+            //StartCoroutine(MoveTowardDirection(5)); 
+            transform.position = pointInTime._position;
+            transform.rotation = pointInTime._rotation;
+            _pointInTime.RemoveAt(0);
 
         }
 
@@ -91,26 +84,38 @@ public class RewindTime : MonoBehaviour
 
     private void FakeReplayPosition()
     {
-
-        //_ballRb.velocity = nextPos; 
-
+        int lastIndex = _pointInTime.Count - 1; 
         
-        //int lastIndex = _pointInTime.Count - 1;
-       
-        
-        //if (Vector3.Distance(_ballRb.velocity,_pointInTime[lastIndex]._position) < 0.1f)
-        //{
-        //    Debug.Log("You've reached your position");
-        //}
-        
-        //for (int i = 0; i < _pointInTime.Count; i++)
-        //{
-        //    Vector3 nextPos = _pointInTime[i]._position;
-        //    Quaternion nextRo = _pointInTime[i]._rotation;
+        if (Vector3.Distance(transform.position, _pointInTime[lastIndex]._position) > 0.1f)
+        {
+            //PointInTime pointInTime = _pointInTime[_indexPosition]; 
 
-        //    _ballRb.velocity = Vector3.Lerp(_ballRb.velocity, nextPos, 1);
-        //}
+            //transform.position = pointInTime._position;
+            //transform.rotation = pointInTime._rotation;
 
+            //Debug.Log("Reached First Position"); 
+
+        }
+        
+
+    }
+
+    IEnumerator MoveTowardDirection(float duration)
+    {
+        PointInTime pointInTime = _pointInTime[_indexPosition];
+
+        while (Vector3.Distance(transform.position, pointInTime._position) > 0.1f)
+        {
+            transform.position = pointInTime._position;
+            transform.rotation = pointInTime._rotation;
+            _indexPosition = Mathf.Clamp(_indexPosition, 0, _pointInTime.Count - 1);
+            yield return new WaitForFixedUpdate();
+        }
+
+        //transform.position = pointInTime._position;
+        //transform.rotation = pointInTime._rotation;
+
+        _indexPosition++;
     }
 
     public void StartRewind()
--- Assets/Script/ScoreManager.cs
+++ Assets/Script/ScoreManager.cs
@@ -43,7 +43,7 @@ public class ScoreManager : MonoBehaviour
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         _instance = FindObjectOfType<ScoreManager>();
         Object.DontDestroyOnLoad(gameObject);
