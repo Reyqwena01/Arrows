9d2aa3f3fe2c194a0d72245a7d6c135918457d2e
Reyqwena01 <157133308+Reyqwena01@users.noreply.github.com>
2025-03-10 16:17:31 +0100

    C'est bon ça ragdoll

--- Assets/Script/EnemyController.cs
+++ Assets/Script/EnemyController.cs
@@ -6,11 +6,12 @@ public class EnemyController : MonoBehaviour
 {
     [SerializeField] private Animator _animator = null;
     [SerializeField] private int _animationSelection = 0;
-    
+
     [SerializeField] private Rigidbody _rb = null;
     [SerializeField] private bool _isTarget = false;
     [SerializeField] private bool _isDead = false;
     [SerializeField] private GameObject[] _bones = null;
+    private Rigidbody[] _rigidbodys = null;
 
     public bool IsDead { get => _isDead; set => _isDead = value; }
 
@@ -54,12 +55,42 @@ public class EnemyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetAllBonesRigidBody();
+        SetRagdollOff();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            SetRagdollOn();
+        }
+
     }
+
+    private void GetAllBonesRigidBody()
+    {
+        _rigidbodys = GetComponentsInChildren<Rigidbody>();
+    }
+
+    public void SetRagdollOn()
+    {
+        for (int i = 0, rL = _rigidbodys.Length; i < rL; i++)
+        {
+            _rigidbodys[i].isKinematic = false;
+        }
+        _animator.enabled = false;
+    }
+
+    public void SetRagdollOff()
+    {
+        int randomValue = Random.Range(0, 3); ;
+        for (int i = 0, rL = _rigidbodys.Length; i < rL; i++)
+        {
+            _rigidbodys[i].isKinematic = true;
+        }
+        _animator.SetInteger("SelectAnimation", randomValue);
+    }
+
 }
