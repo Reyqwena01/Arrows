eeeebbdcc0e72891ec0da0140f0bf77276f86586
Wolprrtinger <162441685+Wolprrtinger@users.noreply.github.com>
2025-04-14 13:26:40 +0200

    Removed duplicates

---
+++ Assets/Script/Controller/CameraController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Physics.Raycast(transform.position, transform.forward, out hit, 1000f);
+    }
+}
